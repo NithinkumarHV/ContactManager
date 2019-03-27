e54820db49bdb418f0164ba921d407b6bd7f528e
Nithin_Kumar_HV <nithinkumarhv01@gmail.com>
2019-03-27 22:12:20 +0530

    first commit

---
+++ src/components/pages/Error.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <h1 className="display-4">404 error</h1>
+      <p className="lead">Error</p>
+    </div>
+  );
+};
